47401202d5a9f8d47d302591c1fec564d79f4212
ManuelRamirez-M <manuel.ramirez@uc.cl>
2023-07-02 19:34:07 -0400

    update

--- start.js
+++ start.js
@@ -2,11 +2,11 @@ const util = require("util");
 const exec = util.promisify(require("child_process").exec);
 
 async function deploy() {
-  const { stdout: output1 } = await exec("npm install");
+  const { stdout: output1 } = await exec("yarn install");
   console.log("Installing dependencies...");
   console.log(output1);
 
-  const { stdout: output2 } = await exec("npm db:migrate");
+  const { stdout: output2 } = await exec("yarn db:migrate");
   console.log("Migrating database...");
   console.log(output2);
 }
